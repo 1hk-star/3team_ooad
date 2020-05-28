ac780d7fa01dd3c03e9c6ffccda0a42fc84a2ab0
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-05-28 20:51:52 +0900

    Signleton Format class
    Format is the "unique" class... so we are gonna use only one object. if you wanna use Format object, you may call getFormat() method... (and It can be called globally..)

--- src/main/java/Format/Format.java
+++ src/main/java/Format/Format.java
@@ -2,6 +2,19 @@ package Format;
 
 public class Format {
 
+
+    private volatile  static Format uniqueInstance;
+
+    /*
+	 volatile 키워드는 다중 스레드 환경에서 한 스레드만 읽기와 쓰기를 하고,
+	 나머지 스레드는 읽기만  할 때 가장 최신의 값을 보장해줍니다..
+
+	 */
+
+
+
+
+
     private int hour;
     private int min;
     private int sec;
@@ -20,6 +33,11 @@ public class Format {
     private int month;
     private int day;
 
+
+    private Format(){
+        //유일성을 보장하기 위해 일부러 private한 것...
+    }
+
     public void setTime(){
 
     }
@@ -32,9 +50,18 @@ public class Format {
 
         return " ";
     }
-    public Format getFormat(){
 
-        return null;
+    public static Format getFormat(){
+        //전역 접근 보장 메소드, Format을 활용하려면 이 메소드를 활용하시면 됩니다.
+
+        if(uniqueInstance==null) {
+            synchronized(Format.class) {
+                if(uniqueInstance==null) {
+                    uniqueInstance=new Format();
+                }
+            }
+        }
+        return uniqueInstance;
     }
 
     public void refreshTime(){}
