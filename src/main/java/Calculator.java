47c618f9a8af63ecb73ed688cab2b7e98b0e9791
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-05-28 11:49:23 +0900

    "commit test of yyj"

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,11 +7,7 @@ public class Calculator {
         return a - b;
     }
     public static void main(String[] args){
-/*<<<<<<< HEAD
-        System.out.println("commit test333");
-=======
-        System.out.println("commit test2");
->>>>>>> c57e8c58fac442f369c959dee3cb0e617a5921c5*/
+System.out.println("dddddddddddddddddd");
     }
 }
 
