43123cafb2787d8cd3e2c4516ac1b028242d8f2a
1hk-star <1hk_star@naver.com>
2020-06-02 23:44:28 +0900

    mTimer 추가
    1234

--- src/main/java/Mode/mTimer.java
+++ src/main/java/Mode/mTimer.java
@@ -1,51 +1,230 @@
 package Mode;
 
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JButton;
 
 import Format.Format;
 import UI.Button;
 
 public class mTimer extends Mode{
+   
+   int flag_set = 0; //       ̴ ,  ƴϴ .
+   int flag_sp = 0; //start면 1 pause면 0
+   int flag_pause = 0;
+   Calendar timer_time = null; // ˶        ð .
+   Calendar pre_time = null; //                    .
+
+   
+   Queue<Integer> cusorQ = new LinkedList<Integer>();
+   private int cur_cursor = -1;
+   
+   Timer m_timer = new Timer();
+   TimerTask m_task = new TimerTask() {
+      @Override
+      public void run() {
+         if(pre_time==null)
+             return;
+         else if(pre_time.get(Calendar.HOUR_OF_DAY)==0&&pre_time.get(Calendar.MINUTE)==0&&
+                pre_time.get(Calendar.SECOND)==0) {
+            flag_pause = 0;
+            pauseTimer();
+             return;
+          }else {
+             flag_pause = 1;
+          }
+          pre_time.add(Calendar.SECOND,-1);
+      }
+   };
+   
+   public mTimer() {
+      cusorQ.offer(3);
+      cusorQ.offer(4);
+      cusorQ.offer(5);
+   }
 
     @Override
     public void work(JButton button) {
-
+       String text = button.getText();
+       if(text.equals("Button1")) {
+          if(flag_set == 0)
+             setTimer();
+          else {
+             moveCursor_timer();
+          }
+       }
+       else if(text.equals("Button2")) {
+          //set Activeif
+          if(flag_set == 1) {
+             plusTimer();
+          }else {
+             if(flag_sp==0)
+                startTimer();
+             else
+                pauseTimer();
+          }
+  
+       }
+       else if(text.equals("Button3")) {
+          //changemode
+          cusorQ.clear();
+          cusorQ.offer(3);
+         cusorQ.offer(4);
+         cusorQ.offer(5);
+          flag_set =0;
+          cur_cursor = -1;
+          timer_time = null;
+       }
+       else if(text.equals("Button4")) {
+          if(flag_set == 1) {
+             confirmTimer();
+          }else {
+             stopTimer();
+          }
+       }
+       else {
+          System.err.println("error button");
+       }
+    }
+    
+    public Calendar getTimer() {
+       if(flag_set == 0)
+          return pre_time;
+       return timer_time;
+    }
+    
+    public int getCursor() {
+       return cur_cursor;
+    }
+    
+    public int get_flag() {
+       return flag_set;
     }
 
-    public Format showTimer(){
-
-        return null;
+    public void showTimer(){
+       
+    }
+    
+    public Calendar getTimerTime() {
+       return pre_time;
+    }
+    public int getPauseFlag() {
+       return flag_pause;
     }
 
     public void startTimer(){
-
+       if(pre_time==null)
+          return;
+       System.out.println("start");
+       flag_sp = 1;
+      m_timer.schedule(m_task,0,1000);
     }
 
     public void stopTimer(){
-
+       pre_time = null;
     }
 
     public void pauseTimer(){
-
+       System.out.println("pause");
+       flag_sp = 0;
+       m_task.cancel();
+       m_timer = new Timer();
+       m_task = new TimerTask() {
+         public void run() {
+            if(pre_time==null)
+                return;
+            else if(pre_time.get(Calendar.HOUR_OF_DAY)==0&&pre_time.get(Calendar.MINUTE)==0&&
+                   pre_time.get(Calendar.SECOND)==0) {
+               flag_pause = 0;
+               pauseTimer();
+                return;
+             } else {
+                flag_pause = 1;
+             }
+             pre_time.add(Calendar.SECOND,-1);
+         }
+       };
     }
 
     public void setTimer(){
-
+       if(flag_set == 0) { //  ð          ߴ .
+          flag_set = 1;
+          cur_cursor = cusorQ.poll(); // ú         ϼ .
+          if(pre_time == null) {
+             timer_time = Calendar.getInstance();
+             timer_time.set(Calendar.HOUR_OF_DAY, 0);
+             timer_time.set(Calendar.MINUTE, 0);
+             timer_time.set(Calendar.SECOND, 0);
+          }
+          else {
+             timer_time = (Calendar)pre_time.clone(); 
+          }
+       }
+       else { //  ð                .
+       }
     }
 
     public void moveCursor_timer(){
-
+       cusorQ.offer(cur_cursor);
+       cur_cursor = cusorQ.poll();
     }
 
     public void plusTimer(){
-
+       switch(cur_cursor) {
+       case 3:
+          int hour = timer_time.get(Calendar.HOUR_OF_DAY);
+          timer_time.set(Calendar.HOUR_OF_DAY, (hour+1)%24);
+       break;
+       case 4:
+          int mit = timer_time.get(Calendar.MINUTE);
+          timer_time.set(Calendar.MINUTE, (mit+1)%60);
+       break;
+       case 5:
+          int sec = timer_time.get(Calendar.SECOND);
+          timer_time.set(Calendar.SECOND, (sec+1)%60);
+       break;
+       default:
+       System.err.println("cursor add error.");
+       break;
+       }
     }
 
     public boolean confirmTimer(){
-
-        return false;
+       //      Ϸ 
+       flag_set = 0;
+       cusorQ.clear();
+      cusorQ.offer(3);
+      cusorQ.offer(4);
+      cusorQ.offer(5);
+       cur_cursor = -1;
+       pre_time = (Calendar) timer_time.clone();
+        return true;
     }
 
+    public void minusTimer(){
+       //System.out.println("cur_cusor: "+cur_cursor);
+       switch(cur_cursor) {
+       case 3:
+          int hour = timer_time.get(Calendar.HOUR_OF_DAY);
+          timer_time.set(Calendar.HOUR_OF_DAY, (hour-1)%24);
+       break;
+       case 4:
+          int mit = timer_time.get(Calendar.MINUTE);
+          timer_time.set(Calendar.MINUTE, (mit-1)%60);
+       break;
+       case 5:
+          int sec = timer_time.get(Calendar.SECOND);
+          timer_time.set(Calendar.SECOND, (sec-1)%60);
+       break;
+       default:
+       System.err.println("cursor add 에러임.");
+       break;
+       }
+    }
 
 
 }
