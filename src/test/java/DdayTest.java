481a171ddc07f928f8650488ed55bbf92423276c
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-06-06 21:30:35 +0900

    add movecursor dday

--- src/test/java/DdayTest.java
+++ src/test/java/DdayTest.java
@@ -26,4 +26,50 @@ class DdayTest {
 		assertEquals("AAA",d.getSetting_page().get_memo());
 	}
 
+	@Test
+	void moveCursor_Dday_Test(){
+		Watch watch=new Watch();
+		Dday d=new Dday();
+
+		//if && if
+		d.setCur_cursor(2);
+
+		d.setString_cur(2);
+
+		d.setFlag_set(1);
+		d.setSetting_page(null);
+
+		JButton button=new JButton();
+		button.setText("Button1");
+		d.work(button);
+
+		assertEquals(0,d.getString_cur());
+		assertEquals(0,d.getCur_cursor());
+		//if&&else
+		d.setCur_cursor(2);
+		d.setString_cur(1);
+		d.setFlag_set(1);
+		d.setSetting_page(null);
+
+
+		d.work(button);
+
+		assertEquals(2,d.getString_cur());
+		assertEquals(2,d.getCur_cursor());
+
+		d.setString_cur(0);
+
+		//else
+
+		d.setCur_cursor(7);
+		d.setFlag_set(1);
+		d.setSetting_page(null);
+
+
+		d.work(button);
+
+		assertEquals(1,d.getCur_cursor());
+
+	}
+
 }
