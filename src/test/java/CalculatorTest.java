c5256b013822fc720a0b84f2205f54b04758b5ee
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-06-06 20:09:25 +0900

    reset test add

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    Calculator c = new Calculator();
-
-    @Test
-    void add() {
-        assertEquals(7,c.add(5,2));
-    }
-
-    @Test
-    void subtract() {
-        assertEquals(3, c.subtract(5, 2));
-        assertEquals(3, c.subtract(5, 2));
-        assertEquals(3, c.subtract(5, 2));
-    }
-
-    @Test
-    void main() {
-    }
+//    Calculator c = new Calculator();
+//
+//    @Test
+//    void add() {
+//        assertEquals(7,c.add(5,2));
+//    }
+//
+//    @Test
+//    void subtract() {
+//        assertEquals(3, c.subtract(5, 2));
+//        assertEquals(3, c.subtract(5, 2));
+//        assertEquals(3, c.subtract(5, 2));
+//    }
+//
+//    @Test
+//    void main() {
+//    }
 }
--- src/test/java/StopWatchTest.java
+++ src/test/java/StopWatchTest.java
@@ -1,12 +1,71 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import Controller.Watch;
+import Mode.StopWatch;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.util.Calendar;
+
 
 class StopWatchTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void resetStopWatchTest() {
+		Watch watch=new Watch();
+		StopWatch st=new StopWatch();
+
+		st.setStart(12);
+		st.setEnd(22);
+		st.setElapse(10);
+		st.setElapsePrevious(1);
+		st.setOn(true);
+
+		Calendar cal=Calendar.getInstance();
+		cal.set(Calendar.HOUR, 9);
+		cal.set(Calendar.MINUTE, 2);
+		cal.set(Calendar.SECOND, 5);
+
+		st.setCal(cal);
+
+		Calendar calLap=Calendar.getInstance();
+		calLap.set(Calendar.HOUR, 5);
+		calLap.set(Calendar.MINUTE, 2);
+		calLap.set(Calendar.SECOND, 2);
+
+		st.setCalLap(calLap);
+
+		assertEquals(12,st.getStart());
+		assertEquals(22,st.getEnd());
+		assertEquals(10,st.getElapse());
+		assertEquals(1,st.getElapsePrevious());
+		assertEquals(true,st.isOn());
+
+		assertEquals(9,st.getCal().get(Calendar.HOUR));
+		assertEquals(2,st.getCal().get(Calendar.MINUTE));
+		assertEquals(5,st.getCal().get(Calendar.SECOND));
+		assertEquals(5,st.getCalLap().get(Calendar.HOUR));
+		assertEquals(2,st.getCalLap().get(Calendar.MINUTE));
+		assertEquals(2,st.getCalLap().get(Calendar.SECOND));
+
+		JButton button=new JButton();
+		button.setText("Button4");
+        st.work(button);
+
+		assertEquals(0,st.getStart());
+		assertEquals(0,st.getEnd());
+		assertEquals(0,st.getElapse());
+		assertEquals(0,st.getElapsePrevious());
+		assertEquals(false,st.isOn());
+
+		assertEquals(0,st.getCal().get(Calendar.HOUR));
+		assertEquals(0,st.getCal().get(Calendar.MINUTE));
+		assertEquals(0,st.getCal().get(Calendar.SECOND));
+		assertEquals(0,st.getCalLap().get(Calendar.HOUR));
+		assertEquals(0,st.getCalLap().get(Calendar.MINUTE));
+		assertEquals(0,st.getCalLap().get(Calendar.SECOND));
+
+
 	}
 
 }
