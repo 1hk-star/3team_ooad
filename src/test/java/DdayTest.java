fc2dbc77d4e37fc4c1c56663179333d74c4525b6
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-06-06 21:17:54 +0900

    SETddayTest add

--- src/test/java/DdayTest.java
+++ src/test/java/DdayTest.java
@@ -1,12 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import Controller.Watch;
+import Mode.Dday;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 class DdayTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void setDdayTest() {
+		Watch watch=new Watch();
+		Dday d=new Dday();
+
+		d.setFlag_set(0);
+		d.setSetting_page(null);
+
+		JButton button=new JButton();
+		button.setText("Button1");
+		d.work(button);
+
+		assertEquals(1,d.getFlag_set());
+		assertEquals(0,d.getCur_cursor());
+
+		assertEquals("AAA",d.getSetting_page().get_memo());
 	}
 
 }
